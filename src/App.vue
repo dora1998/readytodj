35461a1f1bc0a8a7ebe31e74bd803225f6e1fc92
dora <31735614+dora1998@users.noreply.github.com>
2020-11-20 03:42:06 +0900

    tailwindcssを導入

--- src/App.vue
+++ src/App.vue
@@ -2,6 +2,17 @@
   <img alt="Vue logo" src="./assets/logo.png" />
   <HelloWorld msg="Hello Vue 3.0 + Vite" />
   {{ count }}
+  <div class="max-w-sm mx-auto flex p-6 bg-white rounded-lg shadow-xl">
+    <div class="flex-shrink-0">
+      <img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+    </div>
+    <div class="ml-6 pt-1">
+      <h4 class="text-xl text-gray-900 leading-tight">ChitChat</h4>
+      <p class="text-base text-gray-600 leading-normal">
+        You have a new message!
+      </p>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
